8badac801f02489f8e12048dd77e3dbbb5b74585
tie372 <tie.372@gmail.com>
2013-11-24 01:04:02 -0700

    Cleanup server; Start models;

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express.io'),
-    app = express();
+var express = require('express.io');
+var app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
-	res.render('index');
+  res.render('index');
 });
 
 app.listen(6060);
